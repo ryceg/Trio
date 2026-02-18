19dffcf45cb33f5be5df8e9cf21483cead9e14ce
Rhys <rhysg3@gmail.com>
2026-02-19 10:34:25 +1100

    Fix the manual glucose delete

--- Trio/Sources/Services/Network/Nightscout/NightscoutAPI.swift
+++ Trio/Sources/Services/Network/Nightscout/NightscoutAPI.swift
@@ -193,9 +193,9 @@ extension NightscoutAPI {
         components.scheme = url.scheme
         components.host = url.host
         components.port = url.port
-        components.path = Config.treatmentsPath
+        components.path = Config.uploadEntriesPath
         components.queryItems = [
-            URLQueryItem(name: "find[id][$eq]", value: id)
+            URLQueryItem(name: "find[_id][$eq]", value: id)
         ]
 
         guard let url = components.url else {
